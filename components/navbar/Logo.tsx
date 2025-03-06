d8f6e27f3f68fb09f919f0b0f882a9a71c714b09
Martin Ezequiel Williner <williner.dev@gmail.com>
2025-03-06 20:02:15 -0300

    Agregar componente Logo con enlace y botón, e incluir react-icons en las dependencias

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import { SiStylus } from "react-icons/si";
+import Link from 'next/link'
 
 const Logo = () => {
   return (
-    <div>Logo</div>
+    <Button size='icon' asChild>
+      <Link href={'/'}>
+        <SiStylus className='w-6 h-6'/>
+      </Link>
+    </Button>
   )
 }
 
